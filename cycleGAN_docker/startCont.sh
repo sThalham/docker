e354abcb7b40ad4b05525c5994eef8fe6e43ab41
Stefan Thalhammer <stefan.thalhammer136@gmail.com>
2019-06-26 13:35:43 +0200

    python3

--- cycleGAN_docker/startCont.sh
+++ cycleGAN_docker/startCont.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo nvidia-docker build --no-cache -t cyclegan_gpu0 .
-thispid=$(sudo -E NV_GPU=0 nvidia-docker run --name=cyclegan_gpu0 -t -d -v ~/workspace/cyclegan/datasets:/cycleGAN/datasets -p 4000:80 cyclegan_gpu0)
+thispid=$(sudo -E NV_GPU=0 nvidia-docker run --name=cyclegan_gpu0 -t -d -v ~/workspace/cycleGAN/datasets:/cycleGAN/datasets -p 4000:80 cyclegan_gpu0)
 
 #sudo nvidia-docker exec -it $thispid bash
 
